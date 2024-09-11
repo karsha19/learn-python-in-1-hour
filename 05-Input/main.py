dee6c2295f2383ced73267410cbfe1e2a032cff1
Karsha <155376011+karsha19@users.noreply.github.com>
2024-09-11 20:34:04 +0530

    Update main.py

--- 05-Input/main.py
+++ 05-Input/main.py
@@ -1,3 +1,10 @@
 name = input("请输入你的姓名：")
 print("input value: " + name)
 print(type(name))
+#Or We can also use str to represent that input is the string.
+name=str(input("请输入你的姓名：")
+print("input value: " + name)
+print(type(name))
+
+
+      
